9d746c417034a340fd232e745df741f65ebd9b81
Abed Murrar <abedmurrar15@gmail.com>
2019-12-23 10:25:37 +0200

    update redux devtools middleware for browsers that dont support it

--- src/store/store.js
+++ src/store/store.js
@@ -9,8 +9,9 @@ const store = createStore(
     initialState,
     compose(
         applyMiddleware(thunk),
-        window.__REDUX_DEVTOOLS_EXTENSION__ &&
-            window.__REDUX_DEVTOOLS_EXTENSION__()
+        (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ &&
+            window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()) ||
+            compose
     )
 );
 
